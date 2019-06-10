c6623fe3b26795c973ac967b4ad27e585826d255
Stu Blair <stu314159@gmail.com>
2019-06-10 13:13:58 -0500

    introduction of TLBM_Partition object

---
+++ include/TLBM_Partition.h
@@ -0,0 +1,19 @@
+#ifndef TLBM_PARTITION_H
+#define TLBM_PARTITION_H
+#include "Problem.h"
+
+class TLBM_Partition{
+
+  public:
+    Problem thisProblem;
+    TLBM_Partition(int rank, int size);
+    ~TLBM_Partition();
+ 
+
+  private:
+    int rank;
+    int size;
+
+};
+
+#endif
---
+++ src/TLBM_Partition.cpp
@@ -0,0 +1,9 @@
+#include "TLBM_Partition.h"
+
+TLBM_Partition::TLBM_Partition(int r, int s):rank(r),size(s){
+
+}
+
+TLBM_Partition::~TLBM_Partition(){}
+
+
