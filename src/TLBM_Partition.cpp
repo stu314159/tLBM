0b596f15fed7adcc1fc6a58afb835422c0612386
Stu Blair <stu314159@gmail.com>
2019-06-10 13:31:41 -0500

    initial inclusion of TLBM_Partition object

--- src/TLBM_Partition.cpp
+++ src/TLBM_Partition.cpp
@@ -4,6 +4,8 @@ TLBM_Partition::TLBM_Partition(int r, int s):rank(r),size(s){
 
 }
 
-TLBM_Partition::~TLBM_Partition(){}
+TLBM_Partition::~TLBM_Partition(){
+
+}
 
 
--- src/tLBM.cpp
+++ src/tLBM.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio> // sometimes C-style printf works better while using MPI
 #include <mpi.h>
 #include "Problem.h"
 #include "TLBM_Partition.h"
@@ -6,11 +7,12 @@
 int main(int argc, char* argv[]){
   
   int rank, size;
-  MPI_Init(&ragc, &argv);
+  MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD,&size);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-
+  printf("Constructing a partition object for rank %d of %d\n",rank,size);
+  TLBM_Partition myPart(rank,size);
 
   MPI_Finalize();
   return 0;
