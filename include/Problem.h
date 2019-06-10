207b280c10fdb9ba3084e404ceaa69639d282a28
Stu Blair <stu314159@gmail.com>
2019-06-10 11:16:57 -0500

    added code to read params.lbm input file

--- include/Problem.h
+++ include/Problem.h
@@ -6,7 +6,6 @@
 
 #include <string>
 
-
 class Problem{
 
 public:
--- src/Problem.cpp
+++ src/Problem.cpp
@@ -1,5 +1,35 @@
 #include "Problem.h"
+#include <fstream>
+#include <stdexcept>
 
 void Problem::load_input(){
+  ifstream input_params("params.lbm",ios::in);
+  if(!input_params.is_open())
+    throw std::runtime_error("Could not open params.lbm!");
+  
+  input_params >> latticeType;
+  input_params >> dynamics;
+  input_params >> numTs;
+  input_params >> warmupTs;
+  input_params >> plotFreq
+  input_params >> cs;
+  input_params >> rhoLBM;
+  input_params >> uLBM; 
+  input_params >> omega;
+  input_params >> nx;
+  input_params >> ny;
+  input_params >> nz;
+  input_params >> restartFlag;
+  input_params >> timeAvgFlag;
+  input_params >> lx_p;
+  input_params >> ly_p;
+  input_params >> lz_p;
+  input_params >> tConvFact;
+  input_params >> lConvFact;
+  input_params >> pConvFact;
+  input_params >> pRefIdx;
+  input_params >> ssDataFlag;
 
+
+  input_params.close();
 }
