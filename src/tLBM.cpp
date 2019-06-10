cf6726f6cf254cfa1efd1b9d23f599d397fd3cdd
Stu Blair <stu314159@gmail.com>
2019-06-10 13:14:47 -0500

    introduce MPI to tLBM

--- src/tLBM.cpp
+++ src/tLBM.cpp
@@ -3,7 +3,14 @@
 #include "Problem.h"
 
 int main(int argc, char* argv[]){
+  
+  int rank, size;
+  MPI_Init(&ragc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD,&size);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
 
+
+  MPI_Finalize();
   return 0;
 }
