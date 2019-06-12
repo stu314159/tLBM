7886a06fee64490c35b38a1a440f5f4ff63fc730
Stu Blair <stu314159@gmail.com>
2019-06-12 14:19:38 -0500

    added basic D3Q19LatticeStructure object

---
+++ src/D3Q19LatticeStructure.cpp
@@ -0,0 +1,22 @@
+/*
+ * D3Q19LatticeStructure.cpp
+ *
+ *  Created on: Jun 12, 2019
+ *      Author: sblair
+ */
+
+#include "LatticeStructure.h"
+#include "D3Q19LatticeStructure.h"
+
+D3Q19LatticeStructure::D3Q19LatticeStructure():
+LatticeStructure()
+{
+numSpd = 19;
+}
+
+D3Q19LatticeStructure::~D3Q19LatticeStructure(){
+
+}
+
+
+
